69db0664fead101da80f2aebcf29c16842c692a7
Matej Minar <matej.minar@brackets.sk>
2019-06-16 22:51:06 +0200

    craftable installed

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'admin-user' => [
+        'title' => 'Users',
+
+        'actions' => [
+            'index' => 'Users',
+            'create' => 'New User',
+            'edit' => 'Edit :name',
+            'edit_profile' => 'Edit Profile',
+            'edit_password' => 'Edit Password',
+        ],
+
+        'columns' => [
+            'id' => "ID",
+            'first_name' => "First name",
+            'last_name' => "Last name",
+            'email' => "Email",
+            'password' => "Password",
+            'password_repeat' => "Password Confirmation",
+            'activated' => "Activated",
+            'forbidden' => "Forbidden",
+            'language' => "Language",
+                
+            //Belongs to many relations
+            'roles' => "Roles",
+                
+        ],
+    ],
+
+    // Do not delete me :) I'm used for auto-generation
+];
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/* Auto-generated admin routes */
+Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(function () {
+    Route::get('/admin/admin-users',                            'Admin\AdminUsersController@index');
+    Route::get('/admin/admin-users/create',                     'Admin\AdminUsersController@create');
+    Route::post('/admin/admin-users',                           'Admin\AdminUsersController@store');
+    Route::get('/admin/admin-users/{adminUser}/edit',           'Admin\AdminUsersController@edit')->name('admin/admin-users/edit');
+    Route::post('/admin/admin-users/{adminUser}',               'Admin\AdminUsersController@update')->name('admin/admin-users/update');
+    Route::delete('/admin/admin-users/{adminUser}',             'Admin\AdminUsersController@destroy')->name('admin/admin-users/destroy');
+    Route::get('/admin/admin-users/{adminUser}/resend-activation','Admin\AdminUsersController@resendActivationEmail')->name('admin/admin-users/resendActivationEmail');
+});
+
+/* Auto-generated profile routes */
+Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(function () {
+    Route::get('/admin/profile',                                'Admin\ProfileController@editProfile');
+    Route::post('/admin/profile',                               'Admin\ProfileController@updateProfile');
+    Route::get('/admin/password',                               'Admin\ProfileController@editPassword');
+    Route::post('/admin/password',                              'Admin\ProfileController@updatePassword');
+});
