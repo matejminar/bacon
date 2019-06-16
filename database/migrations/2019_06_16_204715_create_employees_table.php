d4859cdfdd3fa62a0cebb3d1a8e26d4fcaed54af
Matej Minar <matej.minar@brackets.sk>
2019-06-16 22:55:27 +0200

    created employees table

---
+++ database/migrations/2019_06_16_204715_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('is_at_work')->default(false);
+            $table->dateTime('last_seen_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
