837b28311e8eddc1089ce64a5438e8073d89fa3e
Matej Minar <matej.minar@brackets.sk>
2019-06-16 23:06:35 +0200

    created devices table

---
+++ database/migrations/2019_06_16_205603_create_devices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->string('mac');
+            $table->string('hostname')->nullable();
+            $table->unsignedInteger('employee_id')->nullable();
+            $table->foreign('employee_id')
+                  ->references('id')
+                  ->on('employees')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
