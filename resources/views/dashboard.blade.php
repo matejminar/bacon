796a8e679fd3adc1da105afb3213c305e721dd63
Matej Minar <matej.minar@brackets.sk>
2019-06-19 01:36:40 +0200

    refreshing of dashboard every 30s

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="refresh" content="30" >
         <title>Bacon</title>
         <link rel="stylesheet" href="{{ mix('/css/main.css') }}">
     </head>
